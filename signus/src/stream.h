85d776451f6358568a8c583d1a70d8d910774cc2
Martin Doucha <doucha@swarmtech.cz>
2021-05-11 23:12:26 +0200

    Add missing licence headers

--- signus/src/stream.h
+++ signus/src/stream.h
@@ -1,3 +1,24 @@
+/*
+ *  This file is part of Signus: The Artefact Wars
+ *  https://github.com/signus-game
+ *
+ *  Copyright (C) 2020-2021 Martin Doucha
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 #ifndef MEMMAN_H_
 #define MEMMAN_H_
 
